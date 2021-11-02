a1341db4554c1ff080a434141ae3035132e57c1f
j.seiffarth <j.seiffarth@fz-juelich.de>
2021-11-02 16:30:06 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 import json
 import mlflow
+from acia.segm.processor.offline import OfflineModel
+import sys
+
+config = sys.argv[2]
+checkpoint = sys.argv[4]
+
+model = OfflineModel(config, checkpoint)
 
 print('Hello mmdetection')
 
